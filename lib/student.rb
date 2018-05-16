7f6d21020fd9d52fab705b19a1001b0ddd30f8e6
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-16 21:07:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ end
 
 #inheriting is not the same as has many relationship
 #inheriting isn't a relationship
-
+#parent to child "but not an Association"
 #one to one - has one, one would have it, belongs to.
 #one to many - one has many and one belongs to.
 #many to many - join table, like has many through a join table.
